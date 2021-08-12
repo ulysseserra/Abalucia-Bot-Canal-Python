253a98345133bdda4d9665d5672e43caa506b68c
Ulysses Serra <72045801+ulysseserra@users.noreply.github.com>
2021-08-12 00:18:52 +0000

    ajuste no evento entrada - recepção do bot

--- index.js
+++ index.js
@@ -6,35 +6,41 @@ app.get("/", (request, response) => {
   console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
   response.sendStatus(200);
 });
+
 app.listen(process.env.PORT); // Recebe solicitações que o deixa online
 
 const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
 const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
 
-client.on("guildMemberAdd", async (member) => { 
 
-  let guild = await client.guilds.cache.get("806968460369985616");
-  let channel = await client.channels.cache.get("806968461401391148");
-  let emoji = await member.guild.emojis.cache.find(emoji => emoji.name === ':pika:');
+client.on("guildMemberAdd", async (member) => {
+
+  let guild = client.guilds.cache.get("806968460369985616");
+  let channel = client.channels.cache.get("806968461401391148");
+  let emoji = member.guild.emojis.cache.find(emoji => emoji.name === 'pika');
+
   if (guild != member.guild) {
     return console.log("Nossas boas-vindas ao Discord da DevAprender!");
-   } else {
-      let embed = await new Discord.MessageEmbed()
-      .setColor("#7c2ae8")
-      .setAuthor(member.user.tag, member.user.displayAvatarURL())
-      .setTitle(`${emoji} Boas-vindas ${emoji}`)
-      .setImage("https://i.gifer.com/9WsF.gif")
-      .setDescription(`**${member.user}**, Bem-vindo(a) ao Servidor **${guild.name}**! Atualmente estamos com **${member.guild.memberCount} membros**, divirta-se conosco! :heart:`)
-      .addFild('Canais', 'Siga as regras do Servidor <#806977330898796545>')
-      .addFild('Cargos', 'Você pode ganhar o cargo: <@&874361381804212235>')
-      .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-      .setFooter('ID do usuário' + member.user.id)
-      .setTimestamp();
+  } else {
+    let embed = await new Discord.MessageEmbed()
+    .setColor('#7c2ae8')
+    .setAuthor(member.user.tag, member.user.displayAvatarURL())
+    .setTitle(`${emoji} Boas-vindas ${emoji}`)
+    .setImage('https://i.gifer.com/9WsF.gif')
+    .setDescription(`**${member.user}**, Boas-vindas ao Servidor **${guild.name}**! Atualmente estamos com **${member.guild.memberCount} membros**, divista-se conosco! :heart:`)
 
-    channel.send(embed);
+    .addFields({name: 'Canais', value: 'Siga as regras do Servidor <#806977330898796545>'})
+    .addFields({name: 'Cargo', value: 'Você pode pegar seu cargo: <#874376805275021312>'})
+
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
+    .setFooter('ID do usuário' + member.user.id)
+    .setTimestamp();
+
+    await channel.send(embed);
+
   }
-});
+})
 
 
 client.on('message', message => {
